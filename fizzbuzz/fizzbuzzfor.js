cd1f41fe5aec9c300558cdab03c62807d7193dab
tashton3 <60495402+tashton3@users.noreply.github.com>
2020-03-17 17:04:20 -0600

    Add files via upload

---
+++ fizzbuzz/fizzbuzzfor.js
@@ -0,0 +1,8 @@
+// ForLoops
+
+for (var x=1; x < 101; x++){
+    if (x % 15 == 0) console.log("FizzBuzz");
+    else if (x % 3 == 0) console.log("Fizz");
+    else if (x % 5 == 0) console.log("Buzz");
+    else console.log(x);
+}
