24ae33c03cc3e89685649311ad0069b92124ddf0
tashton3 <60495402+tashton3@users.noreply.github.com>
2020-03-17 16:49:18 -0600

    Add files via upload

---
+++ fizzbuzzwhile.js
@@ -0,0 +1,17 @@
+let x = 1
+
+while (x<=100 && x>=0){
+ if(x%3 === 0 && x%5 === 0){
+ console.log('FizzBuzz')
+ }
+ else if(x%3 === 0){
+ console.log('Fizz')
+ }
+ else if (x%5 === 0){
+ console.log('Buzz')
+ }
+ else {
+ console.log(x)
+ }
+ x++
+}
